f03262f46eb5605f5d131ef0f776c467389a62f4
Travis Shands <shandstp@gmail.com>
2020-08-14 15:48:04 -0700

    Database driven list

--- wastegram/lib/main.dart
+++ wastegram/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -30,11 +31,18 @@ class WasteList extends StatelessWidget {
         children: [
           Expanded(
             child: Text(
-              document['food'],
-              style: Theme.of(context).textTheme.headline5,
+              DateFormat('EEEE, LLLL d, y')
+                  .format(document['when'].toDate())
+                  .toString(),
+              // document['when'].toDate().toString(),
+              style: Theme.of(context).textTheme.headline6,
             ),
           ),
         ],
+      ),
+      trailing: Expanded(
+        child: Text(document['quantity'].toString(),
+            style: Theme.of(context).textTheme.headline6),
       ),
     );
   }
@@ -46,7 +54,7 @@ class WasteList extends StatelessWidget {
         title: Text(this.title),
       ),
       body: StreamBuilder(
-        stream: Firestore.instance.collection('food').snapshots(),
+        stream: Firestore.instance.collection('waste').snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData) return const CircularProgressIndicator();
           return ListView.builder(
